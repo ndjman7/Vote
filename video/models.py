88e0bc6f9303853c0057a732f49a1f609f31eef3
ndjman7 <ndjman7@gmail.com>
2017-05-20 02:33:11 +0900

    content 모델 추가

--- video/models.py
+++ video/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 200)
+    videoId = models.CharField(max_length = 50)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
