c88b1a56380578664b88b29082516ce620406449
ndjman7 <ndjman7@gmail.com>
2017-05-29 02:38:03 +0900

    random 하게 나오게하기

--- video/views.py
+++ video/views.py
@@ -3,11 +3,11 @@ from .models import Content
 
 def index(request):
     context = {}
-    contents = Content.objects.all()
+    contents = Content.objects.order_by('?')
     rank = Content.objects.order_by('-vote')[:3]
     context['contents'] = contents
     context['rank'] = rank
-    return render(request, 'video/list.html', context)
+    return render(request, 'main/main.html', context)
 
 def detail(request, video_id):
     content = Content.objects.get(pk=video_id)
